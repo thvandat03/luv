999c3662c8fc672cd83fd3ed9361fb76c18a2945
thvandat03 <95754752+thvandat03@users.noreply.github.com>
2022-11-26 16:35:36 +0700

    Add files via upload

---
+++ pft_FullEm/Cnt_DyLve/JS/main.js
@@ -0,0 +1,15 @@
+const startDay = new Date("2022-07-29 00:00:00")
+function clock(){
+    var today = new Date()
+    var Hours = today.getHours();
+    var Minutes = today.getMinutes();
+    var Seconds = today.getSeconds();
+    var time = (Hours<10)?(Hours="0"+Hours):(Hours);
+    (Minutes<10)?(Minutes="0"+Minutes):(Minutes);
+    (Seconds<10)?(Seconds="0"+Seconds):(Seconds);
+    document.querySelector('date').textContent = Math.floor((Math.floor(today - startDay) / 1000 ) / (24*60*60)) ;
+    document.querySelector('.numHours').textContent = Hours;
+    document.querySelector('.numMin').textContent = Minutes;
+    document.querySelector('.numSec').textContent = Seconds;
+}
+setInterval(clock, 1000)
