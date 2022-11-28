eef059e1831b82ae30134968b2207a362f044ca0
thvandat03 <thvandat03@gmail.com>
2022-11-28 23:04:42 +0700

    dat

---
+++ 20-10/js/main.js
@@ -0,0 +1,11 @@
+function play(){
+    var audio = document.getElementById("msc");
+    audio.addEventListener(onload, 'play')
+    audio.loop = true;
+    audio.autoplay = true;
+    audio.autoplay = true;
+    audio.play().innerHTML = x;
+    audio.innerHTML = span
+}
+
+play();
